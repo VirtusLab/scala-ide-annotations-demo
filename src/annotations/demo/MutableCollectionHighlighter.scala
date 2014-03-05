b9325fec96e014ebb9f27b8f6dc7071e62c1961a
Jerzy Müller <Jerzy.Muller@morganstanley.com>
2014-03-05 15:54:20 +0100

    Improvements for new scala-ide

--- src/annotations/demo/MutableCollectionHighlighter.scala
+++ src/annotations/demo/MutableCollectionHighlighter.scala
@@ -1,42 +1,37 @@
 package annotations.demo
 
-import scala.tools.eclipse.semantichighlighting.SemanticHighlightingReconciliation
-import scala.tools.eclipse.semantichighlighting.SemanticHighlightingReconciliationParticipant
-import scala.tools.eclipse.semantichighlighting.custom.AllMethodsTraverserDefinition
-import scala.tools.eclipse.semantichighlighting.custom.CustomSemanticAction
-import scala.tools.eclipse.semantichighlighting.custom.TraverserDefinition
-import scala.tools.eclipse.semantichighlighting.custom.TraverserId
 import MutableCollectionHighlighter._
-import scala.tools.eclipse.semantichighlighting.custom.TraverserDefinition.TypeDefinition
-import preferences.MutableCollectionPreferencePage
-import scala.tools.eclipse.semantic.SemanticAction
 import org.eclipse.jdt.internal.ui.javaeditor.JavaSourceViewer
+import org.scalaide.ui.internal.editor.decorators.custom.AllMethodsTraverserDef
+import org.scalaide.ui.internal.editor.decorators.SemanticAction
+import org.scalaide.ui.internal.editor.decorators.custom.CustomSemanticAction
+import org.scalaide.ui.internal.editor.decorators.custom.TraverserId
+import org.scalaide.ui.internal.editor.decorators.semantichighlighting.SemanticHighlightingReconciliationParticipant
+import org.scalaide.ui.internal.editor.decorators.semantichighlighting.SemanticHighlightingReconciliation
+import org.scalaide.ui.internal.editor.decorators.custom.TraverserDef.TypeDefinition
 
 /**
  * Object with some common properties used by this plugin.
  */
 object MutableCollectionHighlighter {
-  
+
   /** Id of annotation, used in plugin.xml */
-  lazy val ID = "annotations.demo.mutable"
+  lazy val annotationId = "annotations.demo.mutable"
 
   /** Traverser that looks for all method calls on children of collection.mutable.Traversable */
-  lazy val traverser = AllMethodsTraverserDefinition(
-    id = TraverserId(ID),
-    message = "Method call on a mutable collection.",
-    typeDefinition = TypeDefinition("scala" :: "collection" :: "mutable" :: Nil, "Traversable"))
+  lazy val traversers = Seq(
+    AllMethodsTraverserDef(
+      message = "Method call on a mutable collection.",
+      typeDefinition = TypeDefinition("scala" :: "collection" :: "mutable" :: Nil, "Traversable")))
 
-  /** Preference page */
-  lazy val properties = MutableCollectionPreferencePage.properties
-  
   /** Highlighting actions */
   lazy val actions: List[JavaSourceViewer => SemanticAction] =
-    List(viewer => new CustomSemanticAction(viewer, traverser, properties))
+    List(viewer => new CustomSemanticAction(traversers, viewer, annotationId))
 }
 
 /**
  * Custom highlihter for mutable collection methods.
- * 
+ *
  * It's plugged to Scala IDE extension (see plugin.xml).
  */
 class MutableCollectionHighlighter extends SemanticHighlightingReconciliationParticipant(
